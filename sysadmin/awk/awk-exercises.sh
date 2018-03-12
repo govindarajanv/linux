eed14d7a68f21033accf45bb690ef315a18646d1
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-03-12 12:32:39 +0530

    changes done to awk and grep exercises

--- sysadmin/awk/awk-exercises.sh
+++ sysadmin/awk/awk-exercises.sh
@@ -2,4 +2,11 @@
 awk ' { print } ' /etc/ntp.conf
 
 #use begin and end blocks
-awk ' 
+awk ' BEGIN { print "ntp.conf" } { print } END { print NR} ' /etc/ntp.conf 
+
+#print contents with each line bearing a line number
+awk ' BEGIN { print "ntp.conf" } { print NR,$0 } END { print NR} ' /etc/ntp.conf 
+
+#get inet6 from ifconfig
+ifconfig enp0s8|awk -F ":" ' /inet6/ { print $3 $4 $5 }'
+ifconfig enp0s8|awk -F ":" ' /inet6/ { print toupper($3 $4 $5) }'
--- sysadmin/grep/grep-exercises.sh
+++ sysadmin/grep/grep-exercises.sh
@@ -1,2 +1,21 @@
 #Get the function names with lower case characters or _
 declare -f|grep '^[a-z_]'
+
+#get number of packets received and number of bytes transmitted
+ifconfig enps08|grep RX
+
+#get the cpu core details
+grep -c name /proc/cpuinfo
+
+#in the same above command,try to display two lines after the match
+grep -A2 name /proc/cpuinfo
+
+#in the same above command,try to display two lines before the match
+grep -B2 name /proc/cpuinfo
+
+#if you want to print 2 lines before and after the match
+grep -a2 name /proc/cpuinfo
+grep -C2 name /proc/cpuinfo
+
+#parse csv files
+
